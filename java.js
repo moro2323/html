3c7544a5e985033d76744daa3f4afe8847c25ebf
moro2323 <89092937+moro2323@users.noreply.github.com>
2021-08-17 20:19:47 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,19 @@
+// this a commant
+/* also a commant
+* but 4 some reson a *
+*is writen to continue it
+but coutinued*/
+console.log("hello,friend")
+"hello" + "5*10"
+// gives hello5*10
+"hello" + 5*10
+// gives hello50
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
+// returns [w]
+var quote = "Stay awhile and listen!";
+quote.charAt(6);
+"Up up\n\tdown down"
+// Returns:
+Up up
+ down down
